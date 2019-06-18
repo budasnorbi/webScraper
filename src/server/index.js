7243fbd0e616dd3e12e5fe2a35f4d3d18f602fdf
Budás Norbert <budasnorbert@gmail.com>
2019-06-18 20:29:16 +0200

    mindset update

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,25 @@
 const puppeteer = require('puppeteer');
 
+const pricePath = {
+	'compker.hu': 'SPAN.new',
+	'akta-depo.hu': '#page_artdet_price span',
+	'marketworld.hu': 'DIV.listPrice',
+	'firstshop.hu': 'P.price-new',
+	'focuscamera.hu': 'DIV.offer > DIV.price > SPAN.price-new',
+	'rbx.hu': 'P.grossPrice',
+	'oaziscomputer.hu': 'DIV.prices > STRONG',
+	'konzolokszervize.hu': '[id="poduct_current_price"]',
+	'pcx.hu': 'SPAN.price > SPAN',
+	'rufusz.hu': 'DIV.pd-price > SPAN.price-normal',
+	'wincity.hu': 'SPAN.price.price_color.product_table_price',
+	'zonacomputers.hu': 'P.price',
+	'ipon.hu': 'DIV.product__price > DIV.shop-card__price',
+	'pcland.hu': 'DIV.HMi-site-termek-adatlap-ar',
+	'smartshop.hu': 'SPAN.c-product-card__cart__price.ng-star-inserted',
+	'infolex.hu': 'P.termek-adatlap-brutto-ar > STRONG',
+	'konzolvilag.hu': 'DIV.sidebar > DIV.price > DIV.now',
+};
+
 const offers = [
 	{"url":"http://akta-depo.hu/spd/NBL-PU-RED-002/Gamer-szek-Noblechairs-EPIC-Fekete-Piros","elementPath":"#page_artdet_price span"},
 	{"url":"https://compker.hu/termek/34139","elementPath":"SPAN.new"},
@@ -19,8 +39,59 @@ const offers = [
 	{"url":"http://www.infolex.hu/termekek/adatlap/46334/noblechairs-epic-gamer-szek-feketekek","elementPath":"P.termek-adatlap-brutto-ar > STRONG"},
 	{"url":"https://www.konzolvilag.hu/pc/noblechairs-epic-gamer-szek-fekete/piros-nbl-pu-red-002","elementPath":"DIV.sidebar > DIV.price > DIV.now"}
 ];
-
+// Test Page flow
 (async () => {
+	await puppeteer.launch({
+		args: [
+			'--disable-dev-shm-usage',
+			'--unlimited-storage',
+		]
+	});
+	const browser = await puppeteer.launch({ headless: true});
+	const page = await browser.newPage();
+	page.setUserAgent('Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2125.111 Safari/557.36');
+
+	await page.goto('https://irodai-forgoszek.arukereso.hu/noblechairs/epic-nbl-pu-p342158138/');
+	
+	const offerLinks = [
+		...await page.$$eval('#offer-block-promoted > div > a', nodes => nodes.map( node => node.href)),
+		...await page.$$eval('#offer-block-paying > div > a', nodes =>  nodes.map( node => node.href) )
+	];
+
+	page.close();
+
+	const offerRequests = offerLinks.map( offerUrl => async() => {
+		const page = await browser.newPage();
+		page.setUserAgent('Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2125.111 Safari/557.36');
+
+		await page.goto(offerUrl, {timeout: 0});
+
+		const ruleKey = new URL(page.url()).host.replace('www.','');
+		const priceSelector = pricePath[ruleKey];
+		const priceNode = await page.$eval(priceSelector, priceNode => priceNode.textContent);
+		
+		await page.close();
+		return priceNode.replace(/\s/g,'').replace('Ft', '').replace(/\D+/g, '');
+	});
+
+	const prices = [];
+	
+	for (let i = 0; i < offerRequests.length; i++) {
+		try{
+			const price = await offerRequests[i]();
+			console.log(price);
+			prices.push(price);
+		} catch(e) {
+
+		}
+	}
+
+	
+
+})();
+
+
+/*(async () => {
 	const browser = await puppeteer.launch({headless: true});
 	
 	const getOffPrice = async(url , selector) => {
@@ -39,21 +110,20 @@ const offers = [
 
 	for(let offer of offers){
 		try{
-			results.push(
-				await getOffPrice(offer.url, offer.elementPath)
-			);
+			const price = await getOffPrice(offer.url, offer.elementPath);
+			if(price){
+				results.push(price);
+			}
 		}
 		catch(e){
-			console.log(e);
+			// console.log(e);
 		}
 	}
+	await browser.close();
 
 	const bestPrice = results
 		.map( price => parseInt(price))
 		.sort((a,b) => a - b)[0];
 
 	console.log('A legjobb ára a terméknek: ' + bestPrice);
-	console.log(results.length);
-
-  await browser.close();
-})();
+})();*/
